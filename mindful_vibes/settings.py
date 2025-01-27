73f54e6c0568717c02d272a7e66895a18c543ad8
Annie Rhodes <annierhodes17@gmail.com>
2025-01-27 14:31:27 +0000

    restored env varibles, turned debug to FALSE

--- mindful_vibes/settings.py
+++ mindful_vibes/settings.py
@@ -24,9 +24,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # Security settings
 SECRET_KEY = config('SECRET_KEY')
 # SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
-    '8000-annierho7-mindfulvibes-ncqqejxzubn.ws.codeinstitute-ide.net',  # Correctly add the host
+    '8000-annierho7-mindfulvibes-bzlq5f7qhok.ws.codeinstitute-ide.net',
     '.herokuapp.com',
 ]
 
